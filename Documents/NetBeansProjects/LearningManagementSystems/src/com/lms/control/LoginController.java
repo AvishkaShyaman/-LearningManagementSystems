f4136ff37b55230e53bb274017141d0b775ca574
AvishkaShyaman <avishkashyaman@gmail.com>
2020-11-23 20:36:25 +0530

    created project structure and Add sample login UI

---
+++ Documents/NetBeansProjects/LearningManagementSystems/src/com/lms/control/LoginController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lms.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author acer
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField userName;
+    @FXML
+    private PasswordField password;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handelLoginButtonAction(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void handelCancelButtonAction(ActionEvent event) {
+        System.exit(0);
+    }
+    
+}
